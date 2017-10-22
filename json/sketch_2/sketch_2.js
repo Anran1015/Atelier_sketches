5fb2fc5e671d19b561216349c9aaa0bdaca0d9c5
Anran1015 <anranzhou1015@gmail.com>
2017-10-21 20:25:16 -0400

    Update sketch_2.js

--- json/sketch_2/sketch_2.js
+++ json/sketch_2/sketch_2.js
@@ -3,25 +3,23 @@ var flower;
 
 function preload(){
   flower=loadJSON("json.json");
+  //preload the JSON file
 }
 
 function setup(){
 
 createCanvas(700,700);
-
-
-
 }
 
 function draw(){
 background(255);
-fill(flower.r,flower.g,flower.b);
-ellipse(mouseX, mouseY, flower.size,flower.size);
+fill(flower.r,flower.g,flower.b);//value of colour is grabbed from the JSON file
+ellipse(mouseX, mouseY, flower.size,flower.size);// size is created with the data from json file
 
 fill(0);
 var s=map(mouseX, 0,700,10,200);
 textSize(s);
 text(flower.name, mouseX, mouseY);
-
+//showing the name of flower on the top of the circle and change the position with it
 
 }

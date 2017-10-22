15d086b8cb339529da37fb76ff1a3e3575497071
Anran1015 <anranzhou1015@gmail.com>
2017-10-21 20:22:29 -0400

    Update Sound.js

--- Sound/Sound.js
+++ Sound/Sound.js
@@ -4,42 +4,35 @@ var drum;
 
 
 function preload() {
-  // Load a sound file
+  // Load sound files
   song = loadSound('data/videoplayback.mp4');
   drum= loadSound('data/drum.mp4');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-
-  song.loop();
+  song.loop();//background music is looping
 }
 
 function draw() {
 rect(0,0,width,height);
 fill(0);
-  // Set the volume to a range between 0 and 1.0
-  var volume = map(mouseX, 0, width, 0, 1);
-  volume = constrain(volume, 1, 3);
+  // Set the volume to a range between 0 and 1
+  var volume = map(mouseX, 0, width, 1, 3);
+  volume = constrain(volume, 1, 3); //set the volume between 1 and 3
   song.amp(volume);
 
-  // Set the rate to a range between 0.1 and 4
+  // Set the rate to a range between 0.01 and 4
   // Changing the rate alters the pitch
   var speed = map(mouseY, 0.1, height, 0, 2);
   speed = constrain(speed, 0.01, 4);
   song.rate(speed);
 
-  // Draw some circles to show what is going on
+  // Draw some circle to show what is going on
 var m =map(mouseX,0,width,0,255);
-  fill(m,23,100,m );
-var er =map(mouseX,mouseY,width,10,200);
+  fill(m,23,100,m );// the colour of the circle is changing with the mouse position
+var er =map(mouseX,mouseY,width,10,200);// the size of the circle is changing with the mouse position
   ellipse(mouseX, mouseY, er, er);
-  //stroke(0);
-  //fill(51, 100);
-  //ellipse(100, mouseY, 48, 48);
-  
-  
   
   
 }
@@ -47,8 +40,6 @@ var er =map(mouseX,mouseY,width,10,200);
 function keyPressed(){
  if (key=="D") {
       drum.play();
-    
-    rext(0,0,width,height);
-  fill(255);
+    //when key "D" is pressed, play the drum sound effect
     }
 }
